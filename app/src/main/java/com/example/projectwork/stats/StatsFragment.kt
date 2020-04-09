15929c03addeba115ef5df2f24f554a675cf4cc3
DFinageev <finageef@yandex.ru>
2020-04-09 20:53:53 +0300

    stats

--- app/src/main/java/com/example/projectwork/stats/StatsFragment.kt
+++ app/src/main/java/com/example/projectwork/stats/StatsFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.projectwork.R
+import com.example.projectwork.databinding.StatsFragmentBinding
 
 
 class StatsFragment : Fragment() {
@@ -18,6 +19,8 @@ class StatsFragment : Fragment() {
     }
 
     private val viewModel: StatsViewModel by viewModels()
+    //Обьявления обьекта для binding
+    private lateinit var binding : StatsFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +31,11 @@ class StatsFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        binding.apply {
+            statsViewModel = viewModel
+            binding.lifecycleOwner = lifecycleOwner
+        }
 
         requireActivity().onBackPressedDispatcher.addCallback(this) {
             view?.let { findNavController().navigate(R.id.action_statsFragment_to_menuFragment) }
--- app/src/main/java/com/example/projectwork/stats/StatsViewModel.kt
+++ app/src/main/java/com/example/projectwork/stats/StatsViewModel.kt
@@ -1,7 +1,31 @@
 package com.example.projectwork.stats
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.example.projectwork.App
+import com.example.projectwork.database.PolyglotDatabaseDao
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 
-class StatsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class StatsViewModel(app : Application) : AndroidViewModel(app){
+
+    private val database : PolyglotDatabaseDao = (app as App).database.polyglotDatabaseDao
+    private var viewModelJob = Job()
+
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    var mApp = app as App
+    private var okWords = database.getStudiedWords(mApp.currentLanguage)
+    private var allWords = database.getWords(mApp.currentLanguage)
+
+    val studiedWordsAmount: Int = okWords.value?.size ?: 0
+    val wordsAmount: Int = allWords.value?.size ?: 0
+    val wordsLeft: Int = wordsAmount - studiedWordsAmount
+    val studiedWordsAmountString = "Изучено слов: " + studiedWordsAmount.toString()
+    val wordsAmountString = "Всего слов: " + wordsAmount.toString()
+    val wordsLeftString = "Осталось слов: " + wordsLeft.toString()
+
 }
